30983b5d021a78d6bd80c0711ab9bc745d96d478
BryanDLeong <108097095+BryanDLeong@users.noreply.github.com>
2025-07-22 14:23:52 -0700

    Add files via upload

---
+++ black_jack.py
@@ -0,0 +1,21 @@
+import random
+
+# def deal_hand(person):
+#     for i in range(1,3):
+#         person.append(random.choice(cards))
+#         for card in person:
+#             if type(card) == dict:
+#                 person[i-1] = card.keys()
+
+# cards = [1,2,3,4,5,6,7,8,9,10,{"J":10},{"Q":10},{"K":10},{"A":11}]
+
+# dealer_hand = []
+# player_hand = []
+
+# deal_hand(dealer_hand)
+# deal_hand(player_hand)
+# print(dealer_hand)
+test = {}
+
+
+
