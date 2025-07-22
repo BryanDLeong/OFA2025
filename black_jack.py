9676ea7b24a7c3582ea8f6d8405e79fbd77d034d
BryanDLeong <108097095+BryanDLeong@users.noreply.github.com>
2025-07-22 14:27:53 -0700

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -1,21 +1,17 @@
 import random
 
-# def deal_hand(person):
-#     for i in range(1,3):
-#         person.append(random.choice(cards))
-#         for card in person:
-#             if type(card) == dict:
-#                 person[i-1] = card.keys()
+def deal_hand(person):
+    for i in range(1,3):
+        person.append(random.choice(cards))
+        for card in person:
+            if type(card) == dict:
+                person[i-1] = card.keys()
 
-# cards = [1,2,3,4,5,6,7,8,9,10,{"J":10},{"Q":10},{"K":10},{"A":11}]
+cards = [1,2,3,4,5,6,7,8,9,10,{"J":10},{"Q":10},{"K":10},{"A":11}]
 
-# dealer_hand = []
-# player_hand = []
+dealer_hand = []
+player_hand = []
 
-# deal_hand(dealer_hand)
-# deal_hand(player_hand)
-# print(dealer_hand)
-test = {}
-
-
-
+deal_hand(dealer_hand)
+deal_hand(player_hand)
+print(dealer_hand)
